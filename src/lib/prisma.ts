7f9447d677409f970cb6be318cd10b37549f49d3
YourName <your@email.com>
2026-05-09 19:14:04 +0100

    Restore working DB connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = "postgresql://neondb_owner:npg_zTL0CI3vwGXM@ep-cool-salad-aqqrjqlu-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require";
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 export default prisma;
