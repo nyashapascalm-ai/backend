12eb3b62ad8e9fc91a639b334d7749fc65e3a71c
YourName <your@email.com>
2026-05-09 18:56:03 +0100

    Fix database connection for Railway

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import dotenv from "dotenv";
-dotenv.config();
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 export default prisma;
